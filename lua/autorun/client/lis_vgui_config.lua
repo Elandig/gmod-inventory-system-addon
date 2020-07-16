6fb96ae731d77cbc179fc65517f94da5450f199b
Elän <Elandig@users.noreply.github.com>
2020-07-16 23:49:26 +0300

    Updated config

--- lua/autorun/client/lis_vgui_config.lua
+++ lua/autorun/client/lis_vgui_config.lua
@@ -3,16 +3,19 @@
 LIS = LIS or {}
 LIS.CONFIG = {}
 
--- Controls
+-- Controls (client-side)
 LIS.CONFIG.MouseToggle = true -- Allow closing the window by clicking anywhere outside it
+LIS.CONFIG.ShowCloseButton = true -- Allow closing the window by clicking the close button located at the top right corner of it
+LIS.CONFIG.EnableSwitchButton = true -- Allow switching between "compact" and "regular" modes by using the minimize/restore button
 
 -- Appearance
-LIS.CONFIG.PrimaryAccentColor = {66, 66, 72,255} -- Primary Accent Colour
-LIS.CONFIG.SecondaryAccentColor = {66, 66, 72,255} -- Secondary Accent Colour
+LIS.CONFIG.PrimaryAccentColor = {66, 66, 72, 255} -- Primary Accent Colour
+LIS.CONFIG.SecondaryAccentColor = {174, 174, 191, 255} -- Secondary Accent Colour
 
--- Appearance (Individual overwritable parameters if you need more customization)
+-- Appearance (Overwritable variables in case if you need more customization)
 --LIS.CONFIG.BaseWindowBarColor = {66, 66, 72, 255} -- Base Window Bar Colour
 --LIS.CONFIG.BaseWindowBackgroundColor = {66, 66, 72, 217} -- Base Window Background Colour
+--LIS.CONFIG.BaseWindowCloseButtonColor = {174, 174, 191, 170}
 
 -- Animation
 LIS.CONFIG.OpeningAnimationTime = 0.25 -- Open/Close Animation (0 to disable)
