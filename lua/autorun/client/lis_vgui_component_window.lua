4873cdcc7a87742bb853ad303336c3aa1b1b7502
Elän <Elandig@users.noreply.github.com>
2020-07-16 23:50:48 +0300

    Removed useless function. Added Close/Minimize/Restore buttons with temp icons

--- lua/autorun/client/lis_vgui_component_window.lua
+++ lua/autorun/client/lis_vgui_component_window.lua
@@ -2,19 +2,51 @@ local PANEL = {}
 
 function PANEL:Init()
 
+    -- Initialization
 	self:SetTitle( "" )
 	self:ShowCloseButton( false )
     self:SetDraggable( false )
     self:MakePopup()
+    self.Paint = function () end
     
     self.accent_color = {66, 66, 72}
 
     self.background_canvas = vgui.Create("DPanel", self)
     self.bar_canvas = vgui.Create("DPanel", self)
-    self.Paint = function () end
+
+    if LIS.CONFIG.EnableSwitchButton then
+        self.switch_button = vgui.Create("DImageButton", self.bar_canvas)
+    end
+    
+    if LIS.CONFIG.ShowCloseButton then
+        self.close_button = vgui.Create("DImageButton", self.bar_canvas)
+        self.close_button:SetImage("vgui/lis_vgui_close.png")
+        local c = LIS.CONFIG.BaseWindowCloseButtonColor or LIS.CONFIG.SecondaryAccentColor or {143, 174, 191, 170}
+        self.close_button:SetColor(Color(c[1], c[2], c[3], c[4]))
+        self.close_button.DoClick = function()
+            hook.Remove("GUIMousePressed", "LIS_closeCompactInventory")
+            self:Close()
+        end
+    end
 
     self:UpdateRender()
 
+end
+
+function PANEL:EnableWindowSwitchButton(mode)
+
+    if !(mode == "compact" or mode == "regular") then return end
+    if !LIS.CONFIG.EnableSwitchButton then return end
+
+    if mode == "compact" then
+        self.switch_button:SetImage("vgui/lis_vgui_maximize.png")
+    elseif mode == "regular" then
+        self.switch_button:SetImage("vgui/lis_vgui_restore.png")
+    end
+
+    local c = LIS.CONFIG.BaseWindowCloseButtonColor or LIS.CONFIG.SecondaryAccentColor or {143, 174, 191, 170}
+    self.switch_button:SetColor(Color(c[1], c[2], c[3], c[4]))
+    
 end
 
 function PANEL:UpdateRender()
@@ -27,6 +59,21 @@ function PANEL:UpdateRender()
 
     self.bar_canvas:SetPos(0,0)
     self.background_canvas:SetPos(0, rh*27)
+
+    if LIS.CONFIG.ShowCloseButton then
+        self.close_button:SetSize(rw*12,rh*12)
+        self.close_button:SetPos(rw*w-21, rh*7)
+        if LIS.CONFIG.EnableSwitchButton then
+            self.switch_button:SetSize(rw*12,rh*12)
+            self.switch_button:SetPos(rw*w-42,rh*7)
+        end
+    else
+        if LIS.CONFIG.EnableSwitchButton then
+            self.switch_button:SetSize(rw*12,rh*12)
+            self.switch_button:SetPos(rw*w-21,rh*7)
+        end
+    end
+    
 
     local br_color
     if LIS.CONFIG.BaseWindowBarColor then
@@ -63,9 +110,5 @@ function PANEL:SetColor(c)
     self:UpdateRender()
 
 end
-
-function PANEL:GetBase() return self.background_canvas end
-
-function PANEL:GetBarPanel() return self.bar_canvas end
 
 vgui.Register("lis_window_base", PANEL, "DFrame")
