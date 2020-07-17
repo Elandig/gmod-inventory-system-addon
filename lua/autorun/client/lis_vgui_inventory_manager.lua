43e169b73fb3bb9831d63b71171c84ed6699d800
Elän <Elandig@users.noreply.github.com>
2020-07-17 16:40:14 +0300

    Added net.Receive for data recomposition with a new callback system

--- lua/autorun/client/lis_vgui_inventory_manager.lua
+++ lua/autorun/client/lis_vgui_inventory_manager.lua
@@ -1,8 +1,19 @@
 LIS = LIS or {}
 
+local cb_drop_failed
+
 net.Receive("LIS_DrawPlayerInventory", function()
 
     local style,data = net.ReadString(), net.ReadTable()
 
-    LIS.CreateCompactInventory(data)
+    cb_drop_failed = LIS.CreateCompactInventory(data)
+end)
+
+net.Receive("LIS_InventoryItemDropFailed", function()
+
+    local data = net.ReadTable()
+
+    -- Callback from the server to refresh the item list if something went wrong
+    cb_drop_failed(data)
+
 end)
